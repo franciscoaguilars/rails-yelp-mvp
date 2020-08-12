8e4272b571513765e7ca62c347d9d3aaea652c1d
Francisco Aguilar <fiaguilars@gmail.com>
2020-08-12 11:31:24 -0500

    Added db schema plus model valdiation

---
+++ app/models/restaurant.rb
@@ -0,0 +1,7 @@
+class Restaurant < ApplicationRecord
+    has_many :reviews, dependent: :destroy
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: "%{value} is not a valid category" }
+
+end
